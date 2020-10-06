1994c8391e68143b4697c08b9ea826d0acc38c47
GregoriSimei <40555037+GregoriSimei@users.noreply.github.com>
2020-10-05 22:58:16 -0300

    pagina de lista de produtos criada

--- client/src/app/views/prod/list-produto/list-produto.component.ts
+++ client/src/app/views/prod/list-produto/list-produto.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Farmacia } from 'src/app/models/Farmacia';
+import { Produto } from 'src/app/models/Produto';
+import { FarmaService } from 'src/app/services/farma.service';
+import { ProdutoService } from '../../../services/produto.service';
 
 @Component({
   selector: 'app-list-produto',
@@ -7,9 +12,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListProdutoComponent implements OnInit {
 
-  constructor() { }
+  cnpjFarmacia: Number;
+  farmacia: Farmacia;
+  produtos: Produto[] = [];
+
+  constructor(private router: Router, private route: ActivatedRoute, private serviceProd: ProdutoService, private serviceFarma: FarmaService) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => this.cnpjFarmacia = params['cnpj']);
+    this.serviceProd.listar(this.cnpjFarmacia).subscribe((lista) => {
+      this.produtos = lista;
+    });
+  }
+
+  cadastrar() {
+    this.router.navigate(['farma/' + this.cnpjFarmacia + '/cadastrar/produto']);
   }
 
 }
