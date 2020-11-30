3a4980db7f84a4ea1f2919ce7a72cc3e780a6191
GregoriSimei <40555037+GregoriSimei@users.noreply.github.com>
2020-11-30 18:33:15 -0300

    banco de dados

--- server/config/database.js
+++ server/config/database.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 //  mongodb://localhost:27017/TrabDevWeb?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false
 
 
-const db = mongoose.connect("mongodb://localhost:27017/TrabDevWeb?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false", {
+const db = mongoose.connect("mongodb+srv://devwebII:devwebII@gus12345.czwg8.gcp.mongodb.net/MedsOn?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
