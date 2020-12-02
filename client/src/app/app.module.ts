1a096a997f412c6fd5ff164b57bf94e05247910e
NeresDu <eduardoneress12@gmail.com>
2020-12-02 20:09:02 -0300

    alterando parte visual do login

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -30,6 +30,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import {MatCardModule} from '@angular/material/card';
+
 
 
 @NgModule({
@@ -60,7 +62,8 @@ import { MatSelectModule } from '@angular/material/select';
     MatIconModule,
     MatButtonModule,
     MatFormFieldModule,
-    MatSelectModule
+    MatSelectModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
