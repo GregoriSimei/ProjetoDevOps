2a72c0519d039bd33b5cfcd2797cc0ecbc859cfc
Gustavo-RC <gu171994@gmail.com>
2020-12-01 21:45:55 -0300

    Correções e estilização front produtos

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { HeaderComponent } from './components/template/header/header.component';
 import { NavComponent } from './components/template/nav/nav.component';
@@ -19,17 +18,27 @@ import { AlterarFarmaComponent } from './views/farma/alterar-farma/alterar-farma
 import { CreateUserComponent } from './views/auth/create-user/create-user.component';
 import { LoginComponent } from './views/auth/login/login.component';
 
+import localePt from "@angular/common/locales/pt";
+import { registerLocaleData } from "@angular/common";
+registerLocaleData(localePt);
+
 
 // Material Angular
-import { MatTableModule } from '@angular/material/table';
-import { MatInputModule } from '@angular/material/input';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatButtonModule } from "@angular/material/button";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatListModule } from "@angular/material/list";
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatTableModule } from "@angular/material/table";
+import { MatSelectModule } from "@angular/material/select";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatIconModule } from "@angular/material/icon";
+import { LayoutModule } from "@angular/cdk/layout";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
 
 
 @NgModule({
@@ -52,17 +61,27 @@ import { MatSelectModule } from '@angular/material/select';
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatTableModule,
-    MatInputModule,
+    MatButtonModule,
+    MatToolbarModule,
     MatSidenavModule,
     MatListModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
+    MatCardModule,
     MatFormFieldModule,
-    MatSelectModule
+    MatInputModule,
+    MatTableModule,
+    MatSelectModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatIconModule,
+    LayoutModule,
+    MatSnackBarModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: "pt-BR",
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
